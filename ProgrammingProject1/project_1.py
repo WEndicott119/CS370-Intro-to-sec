b6860b3976d9341838fb3780920d8326c87fa483
William Endicott <endicotw@oregonstate.edu>
2022-04-21 14:56:59 -0700

    testing things and trying to figure out how to import large file with 600k entrys to fill each filter

--- ProgrammingProject1/project_1.py
+++ ProgrammingProject1/project_1.py
@@ -2,7 +2,7 @@ import hashlib
 from typing import Counter
 
 # create an array to hold results
-bloom3 = [0] * 20
+bloom3 = [0] * 10
 print(bloom3)
 
 bloom5 = [0] * 10
@@ -12,16 +12,22 @@ print(bloom5)
 def compute_sha256_hash(string):
 
     sha256_hash = int(hashlib.sha256(
-        string.encode("utf-8")).hexdigest(), 16) % (10 ** 4)
+        string.encode("utf-8")).hexdigest(), 16) % (10 ** 1)
     print("sha256: ", sha256_hash)
+    if bloom3[sha256_hash] == 0:
+        bloom3[sha256_hash] = 1
 
-    mds_hash = int(hashlib.md5(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 4)
-    print("md5: ", mds_hash)
+    md5_hash = int(hashlib.md5(string.encode(
+        "utf-8")).hexdigest(), 16) % (10 ** 1)
+    print("md5: ", md5_hash)
+    if bloom3[md5_hash] == 0:
+        bloom3[md5_hash] = 1
 
     sha1_hash = int(hashlib.sha1(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 4)
+        "utf-8")).hexdigest(), 16) % (10 ** 1)
     print("sha1: ", sha1_hash)
+    if bloom3[sha1_hash] == 0:
+        bloom3[sha1_hash] = 1
 
 
 #file_name = input("file name: ")
@@ -29,5 +35,7 @@ def compute_sha256_hash(string):
 with open("sample_input.txt", 'r') as file:
     for file_contents in file:
         compute_sha256_hash(file_contents)
+
+print(bloom3)
 
 file.close()
