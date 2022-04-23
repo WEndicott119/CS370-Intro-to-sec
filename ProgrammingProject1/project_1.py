e78428ce71eaf948466f0a4b653a6f7ae54ba8d6
William Endicott <endicotw@oregonstate.edu>
2022-04-22 17:24:55 -0700

    figured out how to populate the BF.

--- ProgrammingProject1/project_1.py
+++ ProgrammingProject1/project_1.py
@@ -1,40 +1,44 @@
 import hashlib
-from typing import Counter
 
 # create an array to hold results
-bloom3 = [0] * 10
-print(bloom3)
+bloom3 = [0] * 1000000
+# print(bloom3)
 
-bloom5 = [0] * 10
-print(bloom5)
+bloom5 = [0] * 1000000
+# print(bloom5)
 
 
-def compute_sha256_hash(string):
+def check_hash(string):
 
     sha256_hash = int(hashlib.sha256(
-        string.encode("utf-8")).hexdigest(), 16) % (10 ** 1)
+        string.encode("utf-8")).hexdigest(), 16) % (10 ** 6)
     print("sha256: ", sha256_hash)
+
+    md5_hash = int(hashlib.md5(string.encode(
+        "utf-8")).hexdigest(), 16) % (10 ** 6)
+    print("md5: ", md5_hash)
+
+    sha1_hash = int(hashlib.sha1(string.encode(
+        "utf-8")).hexdigest(), 16) % (10 ** 6)
+    print("sha1: ", sha1_hash)
+
+    if bloom3[sha256_hash] == 1 & bloom3[md5_hash] == 1 & bloom3[sha1_hash] == 1:
+        print("That password maybe no good.")
+
     if bloom3[sha256_hash] == 0:
         bloom3[sha256_hash] = 1
 
-    md5_hash = int(hashlib.md5(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 1)
-    print("md5: ", md5_hash)
     if bloom3[md5_hash] == 0:
         bloom3[md5_hash] = 1
 
-    sha1_hash = int(hashlib.sha1(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 1)
-    print("sha1: ", sha1_hash)
     if bloom3[sha1_hash] == 0:
         bloom3[sha1_hash] = 1
 
 
-#file_name = input("file name: ")
-# file_name
-with open("sample_input.txt", 'r') as file:
+with open("dictionary2.txt", 'r') as file:
     for file_contents in file:
-        compute_sha256_hash(file_contents)
+        check_hash(file_contents)
+
 
 print(bloom3)
 
