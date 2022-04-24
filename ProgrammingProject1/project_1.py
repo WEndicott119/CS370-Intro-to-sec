43aed7a87a850019e4419da77d9ec1e63bd7c9df
William Endicott <endicotw@oregonstate.edu>
2022-04-23 21:52:20 -0700

    both bloom filter output the same results so something is funky.

--- ProgrammingProject1/project_1.py
+++ ProgrammingProject1/project_1.py
@@ -8,8 +8,8 @@ import hashlib
 import sys
 
 # create an array to hold results
-bloom3 = [0] * 100000000
-bloom5 = [0] * 100000000
+bloom3 = [0] * 1000000000
+bloom5 = [0] * 1000000000
 
 # converts each line into hash and then inputs them into the bloom filter
 
@@ -17,51 +17,51 @@ bloom5 = [0] * 100000000
 def pop_bloom(string):
 
     sha256_hash = int(hashlib.sha256(
-        string.encode("utf-8")).hexdigest(), 16) % (10 ** 8)
+        string.encode("utf-8")).hexdigest(), 16) % (1000000000)
     # print("sha256: ", sha256_hash)
     bloom3[sha256_hash] = 1
     bloom5[sha256_hash] = 1
 
     md5_hash = int(hashlib.md5(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 8)
+        "utf-8")).hexdigest(), 16) % (1000000000)
     # print("md5: ", md5_hash)
     bloom3[md5_hash] = 1
     bloom5[md5_hash] = 1
 
     sha1_hash = int(hashlib.sha1(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 8)
+        "utf-8")).hexdigest(), 16) % (1000000000)
     # print("sha1: ", sha1_hash)
     bloom3[sha1_hash] = 1
     bloom5[sha1_hash] = 1
 
     sha224_hash = int(hashlib.sha224(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 8)
+        "utf-8")).hexdigest(), 16) % (1000000000)
     # print("sha224: ", sha224_hash)
     bloom5[sha224_hash] = 1
 
     sha384_hash = int(hashlib.sha384(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 8)
+        "utf-8")).hexdigest(), 16) % (1000000000)
     # print("sha384: ", sha384_hash)
     bloom5[sha384_hash] = 1
 
 
 def check_user_3(string):
     sha256_hash = int(hashlib.sha256(
-        string.encode("utf-8")).hexdigest(), 16) % (10 ** 8)
+        string.encode("utf-8")).hexdigest(), 16) % (1000000000)
     # print("sha256: ", sha256_hash)
 
     md5_hash = int(hashlib.md5(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 8)
+        "utf-8")).hexdigest(), 16) % (1000000000)
     # print("md5: ", md5_hash)
 
     sha1_hash = int(hashlib.sha1(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 8)
+        "utf-8")).hexdigest(), 16) % (1000000000)
     # print("sha1: ", sha1_hash)
 
     if bloom3[sha256_hash] == 1 & bloom3[md5_hash] == 1 & bloom3[sha1_hash] == 1:
-        result = " Maybe"
+        result = "Maybe     "
     else:
-        result = " No"
+        result = "No    "
 
     # checking each hash against what has been seen before
     if bloom3[sha256_hash] == 0:
@@ -73,34 +73,37 @@ def check_user_3(string):
     if bloom3[sha1_hash] == 0:
         bloom3[sha1_hash] = 1
 
-    print(string + " " + result)
+    file = open('output3.txt', 'a')
+
+    file.write(result)
+    file.write(string)
 
 
 def check_user_5(string):
     sha256_hash = int(hashlib.sha256(
-        string.encode("utf-8")).hexdigest(), 16) % (10 ** 8)
+        string.encode("utf-8")).hexdigest(), 16) % (1000000000)
     # print("sha256: ", sha256_hash)
 
     md5_hash = int(hashlib.md5(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 8)
+        "utf-8")).hexdigest(), 16) % (1000000000)
     # print("md5: ", md5_hash)
 
     sha1_hash = int(hashlib.sha1(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 8)
+        "utf-8")).hexdigest(), 16) % (1000000000)
     # print("sha1: ", sha1_hash)
 
     sha224_hash = int(hashlib.sha224(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 8)
+        "utf-8")).hexdigest(), 16) % (1000000000)
     # print("sha224: ", sha224_hash)
 
     sha384_hash = int(hashlib.sha384(string.encode(
-        "utf-8")).hexdigest(), 16) % (10 ** 8)
+        "utf-8")).hexdigest(), 16) % (1000000000)
     # print("sha384: ", sha384_hash)
 
     if bloom5[sha256_hash] == 1 & bloom5[md5_hash] == 1 & bloom5[sha1_hash] == 1 & bloom5[sha224_hash] == 1 & bloom5[sha384_hash] == 1:
-        result = " Maybe"
+        result = "Maybe     "
     else:
-        result = " No"
+        result = "No    "
 
     # checking each hash against what has been seen before
     if bloom5[sha256_hash] == 0:
@@ -118,7 +121,10 @@ def check_user_5(string):
     if bloom5[sha384_hash] == 0:
         bloom5[sha384_hash] = 1
 
-    print(string + " " + result)
+    file = open('output5.txt', 'a')
+
+    file.write(result)
+    file.write(string)
 
 
 def read_file_contents(string):
@@ -133,11 +139,11 @@ def read_file_contents(string):
 def read_user_input(string):
 
     with open(string, 'r') as file:
+        next(file)
         for file_contents in file:
+            file_contents.strip()
             check_user_5(file_contents)
-            #print(file_contents, bloom5)
             check_user_3(file_contents)
-            #print(file_contents, bloom3)
 
     file.close()
 
@@ -145,9 +151,3 @@ def read_user_input(string):
 # takes the arguments at run time and for the file.
 read_file_contents(sys.argv[1])
 read_user_input(sys.argv[2])
-# print(bloom3)
-
-# way to calculate how big each bloom filter should be.
-# way to tell the user if the password passes the bloom filter or has been seen maybe
-# wrtie to a file the output of the users file input
-# Need to add two more hash functions and a way to input data into both bloom filters
